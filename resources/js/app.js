1cd265d1022f43d2af8f6679a1de89c705b29eef
Md. Nurul Momen <mdnurulmomen.bd@gmail.com>
2020-02-19 15:59:53 +0600

    Server side pagination handled with vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,7 @@
 
 window.Vue = require('vue');
 
+Vue.component('pagination', require('./components/admin/PaginationComponent.vue').default);
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
