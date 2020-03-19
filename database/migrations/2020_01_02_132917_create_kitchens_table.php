6ca840ebdd8ca3a00c36e5c9a22fb4bfa3d3fff7
Md. Nurul Momen <mdnurulmomen.bd@gmail.com>
2020-03-19 17:43:26 +0600

    Missing part of Kitchen CRUD

---
+++ database/migrations/2020_01_02_132917_create_kitchens_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKitchensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kitchens', function (Blueprint $table) {
+            $table->mediumIncrements('id');
+            $table->string('user_name', 30)->unique(); // phone or user_name
+            $table->string('mobile', 13)->unique();
+            $table->string('email')->nullable()->unique();
+            $table->string('password'); // Login with username / mobile
+            $table->unsignedMediumInteger('restaurant_id')->unique(); //Each restaurant has one kitchen
+            $table->rememberToken();
+            $table->boolean('admin_approval')->default(true);
+
+            $table->softDeletes();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kitchens');
+    }
+}
