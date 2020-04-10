dfab6cfbb9a2d4fddc9a1f9215d06f6f73179a24
Md. Nurul Momen <mdnurulmomen.bd@gmail.com>
2020-04-10 11:24:56 +0600

    New route for clearing all cache

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,14 @@ Route::view('/', 'layouts.website')->name('website');
 
 Route::get('/home', 'Auth\HomeController@index')->name('customer.home');
 
+Route::get('/clear-all-cache', function() {
+    Artisan::call('cache:clear');
+    Artisan::call('route:clear');
+    Artisan::call('config:clear');
+    Artisan::call('view:clear');
+    return "All cache is cleared";
+});
+
 Route::fallback(function () {
     Route::view('/', 'layouts.website');
 });
