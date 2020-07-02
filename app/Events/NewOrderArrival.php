dc742d5a204ae153670cea735f83d17b4f5d21b7
Md. Nurul Momen <mdnurulmomen.bd@gmail.com>
2020-07-02 12:35:04 +0600

    'orders' API (modifying event payload)

--- app/Events/NewOrderArrival.php
+++ app/Events/NewOrderArrival.php
@@ -29,6 +29,40 @@ class NewOrderArrival implements ShouldBroadcastNow
     }
 
     /**
+     * Get the data to broadcast.
+     *
+     * @return array
+     */
+    public function broadcastWith()
+    {
+        return [
+            'id' => $this->order->id,
+            'order_type' => $this->order->order_type,
+            'is_asap_order' => $this->order->is_asap_order,
+            'delivery_datetime' => $this->order->delivery_datetime,
+            'order_price' => $this->order->order_price,
+            'vat' => $this->order->vat,
+            'discount' => $this->order->discount,
+            'delivery_fee' => $this->order->delivery_fee,
+            'net_payable' => $this->order->net_payable,
+            'payment_method' => $this->order->payment_method,
+            'cutlery_addition' => $this->order->cutlery_addition,
+            'orderer_type' => $this->order->orderer_type,
+            'orderer_id' => $this->order->orderer_id,
+            'call_confirmation' => $this->order->call_confirmation ?? -1,
+            'created_at' => $this->order->created_at,
+            'updated_at' => $this->order->updated_at,
+            'order_ready_confirmations' => $this->order->orderReadyConfirmations,
+            'orderer' => $this->order->orderer,
+            'restaurant_acceptances' => $this->order->restaurantAcceptances,
+            'rider_assignment' => $this->order->riderAssignment,
+            'rider_delivery_confirmation' => $this->order->riderDeliveryConfirmation,
+            'rider_food_pick_confirmations' => $this->order->riderFoodPickConfirmations,
+            'waiter_serve_confirmation' => $this->order->waiterServeConfirmation
+        ];
+    }
+
+    /**
      * Get the channels the event should broadcast on.
      *
      * @return \Illuminate\Broadcasting\Channel|array
