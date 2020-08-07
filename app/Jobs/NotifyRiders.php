8f76d8815d5e8877fa4d83d918038301aafad473
Md. Nurul Momen <mdnurulmomen.bd@gmail.com>
2020-08-07 17:16:06 +0600

    Job for Rider-Broadcasting

---
+++ app/Jobs/NotifyRiders.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Jobs;
+
+use Exception;
+use App\Models\Order;
+use App\Models\Rider;
+use App\Events\UpdateRider;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+// use Illuminate\Support\Facades\Log;
+
+class NotifyRiders implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $order, $rider;
+
+    /**
+     * Delete the job if its models no longer exist.
+     *
+     * @var bool
+     */
+    public $deleteWhenMissingModels = true;
+
+    /**
+     * The number of times the job may be attempted.
+     *
+     * @var int
+     */
+    public $tries = 25;
+
+    /**
+     * The maximum number of exceptions to allow before failing.
+     *
+     * @var int
+     */
+    // public $maxExceptions = 5;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Order $order, Rider $rider)
+    {
+       $this->order = $order; 
+       $this->rider = $rider; 
+    }
+
+    /**
+     * Determine the time at which the job should timeout.
+     *
+     * @return \DateTime
+     */
+/*
+    public function retryUntil()
+    {
+        return now()->addSeconds(30);
+    }
+*/
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {  
+        if (!$this->order->riderAssignment()->exists()) {
+            
+            // initially cancelling order delivery request
+            $riderNewDeliveryRecord = $this->order->riderAssignment()->create([
+                'delivery_order_acceptance' => 0,
+                'rider_id' => $this->rider->id
+            ]);
+
+            // setting paused time to related rider for this cancelled delivery request
+            $riderNewDeliveryRecord->rider()->update([
+                'paused_at' => now()
+            ]);
+
+            // Notify Riders
+            // Log::warning('NotifyRiders to Rider id : '.$this->rider->id.', at : '.date("h:i:sa"));
+
+            // Broadcast to Rider for order request
+            event(new UpdateRider($riderNewDeliveryRecord));
+            
+        }
+    }
+
+    /**
+     * Handle a job failure.
+     *
+     * @param  \Exception  $exception
+     * @return void
+     */
+    public function failed(Exception $exception)
+    {
+        // Send user notification of failure, etc...
+    }
+
+}
